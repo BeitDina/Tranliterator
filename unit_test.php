14ed8b3f72fd57bf4c8975ccb149471242d6f13d
Florin-Ciprian Bodin <orynider@rdslink.ro>
2023-10-12 04:44:50 +0300

    Update unit_test.php
    Updated to PHP 8+

--- unit_test.php
+++ unit_test.php
@@ -1,8 +1,8 @@
 <?php
-	include 'trans.php';
-	//$sourcetext = "מוֹל סוּף";
-	$sourcetext = "&#1502;&#1493;&#1465;&#1500; &#1505;&#1493;&#1468;&#1507;"; // mol suf	
+	$phpEx = 'php'; $root_path = "./";
+	include $root_path.'trans.'.$phpEx;
+	$sourcetext = 'מוֹל סוּף'; //$sourcetext = "&#1502;&#1493;&#1465;&#1500; &#1505;&#1493;&#1468;&#1507;"; // mol suf	
 	$targetlang = "ashkenazic";
-	echo generateTransliteration($sourcetext, $targetlang, FALSE, TRUE);
-	
+	print '<br><br> i.e. Source text: ' . $sourcetext . '.
+	<br>See result above: <br>'. generateTransliteration($sourcetext, $targetlang, FALSE, TRUE);
 ?>
